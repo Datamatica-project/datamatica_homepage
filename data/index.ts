e1512c8af09be3f89b4bf8d49906f613622b1e18
jonghyun <mattew1803@naver.com>
2026-03-06 18:06:37 +0900

    ✅이미지 추가가

---
+++ data/index.ts
@@ -0,0 +1,39 @@
+interface skillDataProps {
+  title: string;
+  description: string;
+  imageSrc: string;
+}
+
+// 기술 데이터
+export const skillData: skillDataProps[] = [
+  {
+    title: "자율주행",
+    description: "센서·AI 기반의<br /> 안전하고 지능적인 주행 기술.",
+    imageSrc: "/business/AM.png",
+  },
+  {
+    title: "인공지능 데이터 구축",
+    description: "AI 학습을 위한<br /> 고품질 데이터 구축 솔루션.",
+    imageSrc: "/business/Labeling.png",
+  },
+  {
+    title: "스마트 안전 관리 시스템",
+    description: "IoT 센서 기반 실시간<br /> 위험 감지 및 대응 플랫폼.",
+    imageSrc: "/business/Safty.png",
+  },
+  {
+    title: "스마트 헬스케어 플랫폼",
+    description: "지속 가능한 건강 관리와<br /> 데이터 기반 의료 솔루션.",
+    imageSrc: "/business/AM.png",
+  },
+  {
+    title: "디지털 트윈",
+    description: "현실을 복제한 가상 시뮬레이션<br /> 기반 예측 시스템.",
+    imageSrc: "/business/AM.png",
+  },
+  {
+    title: "고정밀 지도",
+    description: "정밀 3D 공간 데이터로<br /> 자율주행의 정확도를 완성합니다.",
+    imageSrc: "/business/AM.png",
+  },
+];
